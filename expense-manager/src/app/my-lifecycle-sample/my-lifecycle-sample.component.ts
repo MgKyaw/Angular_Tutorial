fa3499521b828dc00f45c19ecceb8e3058e18953
MgKyaw <mgkyaw20112@gmail.com>
2025-03-21 20:44:33 +0700

    feat: Add all lifecycle hooks

--- expense-manager/src/app/my-lifecycle-sample/my-lifecycle-sample.component.ts
+++ expense-manager/src/app/my-lifecycle-sample/my-lifecycle-sample.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'app-my-lifecycle-sample',
-  imports: [],
-  templateUrl: './my-lifecycle-sample.component.html',
-  styleUrl: './my-lifecycle-sample.component.css'
+   selector: 'app-my-lifecycle-sample',
+   templateUrl: './my-lifecycle-sample.component.html',
+   styleUrls: ['./my-lifecycle-sample.component.css']
 })
-export class MyLifecycleSampleComponent {
+export class MyLifecycleSampleComponent implements OnChanges, OnInit {
+
+   ngOnChanges() {
+      console.log("Change detection")
+   }
+
+   ngOnInit() {
+      console.log("Initialization of component / directive")
+   }
+   
+   ngDoCheck() {
+      console.log("Custom change detection")
+   }
+   
+   ngAfterContentInit() {
+      console.log("Content initialization")
+   }
+   
+   ngAfterContentChecked() {
+      console.log("Checking changes in content")
+   }
+   
+   ngAfterViewInit() {
+      console.log("View initialization")
+   }
+   
+   ngAfterViewChecked() {
+      console.log("Checking changes in views")
+   }
+   
+   ngOnDestroy() {
+      console.log("Destruction of component / directive")
+   }
 
 }
