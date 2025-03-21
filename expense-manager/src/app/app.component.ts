afbeb1c19cc139621c9e8eafef242130b1d81521
MgKyaw <mgkyaw20112@gmail.com>
2025-03-21 22:30:28 +0700

    feat: Import MyLifecycleSampleComponent

--- expense-manager/src/app/app.component.ts
+++ expense-manager/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+import { MyLifecycleSampleComponent } from './my-lifecycle-sample/my-lifecycle-sample.component';
 
 @Component({
  selector: 'app-root',
  standalone: true,
- imports: [RouterOutlet, ExpenseEntryComponent],
+ imports: [RouterOutlet, ExpenseEntryComponent, MyLifecycleSampleComponent],
  templateUrl: './app.component.html',
  styleUrl: './app.component.css'
 })
