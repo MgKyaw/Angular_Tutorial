8de8314dfb7fa88e9d934d16d1ef39c4f720d5a7
MgKyaw <mgkyaw20112@gmail.com>
2025-03-22 21:15:57 +0700

    feat: Render new component

--- sample-app/src/app/app.component.ts
+++ sample-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewEncapsulationSampleComponent } from './view-encapsulation-sample/view-encapsulation-sample.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ViewEncapsulationSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
