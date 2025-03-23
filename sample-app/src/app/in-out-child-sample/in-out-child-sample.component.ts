1b2fefa5592d90abc80f69450a40e57ff17976f7
MgKyaw <mgkyaw20112@gmail.com>
2025-03-23 11:46:38 +0700

    feat: Generate new component for Auto interception

---
+++ sample-app/src/app/in-out-child-sample/in-out-child-sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-in-out-child-sample',
+  imports: [],
+  templateUrl: './in-out-child-sample.component.html',
+  styleUrl: './in-out-child-sample.component.css'
+})
+export class InOutChildSampleComponent {
+
+}
