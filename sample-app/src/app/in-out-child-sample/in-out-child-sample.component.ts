f8a69595ab946e3ee34a2da80018c609e70ab138
MgKyaw <mgkyaw20112@gmail.com>
2025-03-23 16:31:27 +0700

    feat: Use Output decorator

--- sample-app/src/app/in-out-child-sample/in-out-child-sample.component.ts
+++ sample-app/src/app/in-out-child-sample/in-out-child-sample.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-in-out-child-sample',
@@ -7,11 +7,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './in-out-child-sample.component.css'
 })
 export class InOutChildSampleComponent {
-  @Input()
-   get counter(): number { return this._counter; }
-   set counter(val: number) {
-      this._counter = val || 0;
-      if(val > 25) this._counter = val % 25;
-   }
-   private _counter: number = 1;
+  @Input() counter : number = 0;
+  @Output() counterEvent = new EventEmitter<number>();
+  passCounterToParent() {
+    this.counterEvent.emit(this.counter)
+ }
 }
