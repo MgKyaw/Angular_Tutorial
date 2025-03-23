f932c2ec8e4a5179e745460d6b3ff28700755b9d
MgKyaw <mgkyaw20112@gmail.com>
2025-03-23 11:51:25 +0700

    feat: Import InOutChildSampleComponent

--- sample-app/src/app/app.component.ts
+++ sample-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ViewEncapsulationSampleComponent } from './view-encapsulation-sample/view-encapsulation-sample.component';
+import { InOutChildSampleComponent } from './in-out-child-sample/in-out-child-sample.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ViewEncapsulationSampleComponent],
+  imports: [RouterOutlet, ViewEncapsulationSampleComponent, InOutChildSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
