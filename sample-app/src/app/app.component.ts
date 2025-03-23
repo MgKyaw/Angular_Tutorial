c5680da82b26385e1c7c79e2768ff9d91a6d9b66
MgKyaw <mgkyaw20112@gmail.com>
2025-03-23 16:48:57 +0700

    feat: Import ChildCounter component

--- sample-app/src/app/app.component.ts
+++ sample-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ViewEncapsulationSampleComponent } from './view-encapsulation-sample/view-encapsulation-sample.component';
 import { InOutChildSampleComponent } from './in-out-child-sample/in-out-child-sample.component';
+import { ChildCounterComponent } from './child-counter/child-counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ViewEncapsulationSampleComponent, InOutChildSampleComponent],
+  imports: [RouterOutlet, ViewEncapsulationSampleComponent, InOutChildSampleComponent, ChildCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
