a5130dd21b1ee7319730885667ce898857cdcf12
MgKyaw <mgkyaw20112@gmail.com>
2025-03-23 16:43:01 +0700

    feat: Add ChildCounter component

---
+++ sample-app/src/app/child-counter/child-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-counter',
+  imports: [],
+  templateUrl: './child-counter.component.html',
+  styleUrl: './child-counter.component.css'
+})
+export class ChildCounterComponent {
+
+}
