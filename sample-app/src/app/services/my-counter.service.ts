b8481db3e239a2ba6e51a5f3f453e44aaf502fdf
MgKyaw <mgkyaw20112@gmail.com>
2025-03-25 22:19:44 +0700

    feat: Create an observable object

--- sample-app/src/app/services/my-counter.service.ts
+++ sample-app/src/app/services/my-counter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,7 @@ import { Injectable } from '@angular/core';
 export class MyCounterService {
 
   constructor() { }
+  
+  private counterSource = new Subject<number>();
+  public counter$ = this.counterSource.asObservable()
 }
