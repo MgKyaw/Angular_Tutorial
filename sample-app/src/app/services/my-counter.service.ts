dfa46eae92076029c6f36856f4b9160bcd4f3bc8
MgKyaw <mgkyaw20112@gmail.com>
2025-03-25 22:20:31 +0700

    feat: Implement inc and dec methods

--- sample-app/src/app/services/my-counter.service.ts
+++ sample-app/src/app/services/my-counter.service.ts
@@ -7,7 +7,10 @@ import { Subject } from 'rxjs';
 export class MyCounterService {
 
   constructor() { }
-  
+
   private counterSource = new Subject<number>();
   public counter$ = this.counterSource.asObservable()
+
+  inc(val: number) { this.counterSource.next(val + 1) }
+  dec(val: number) { this.counterSource.next(val - 1) }
 }
