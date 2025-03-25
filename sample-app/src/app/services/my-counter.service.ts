fbe0731c2c05490d81a16aa5973d2242a3b55fea
MgKyaw <mgkyaw20112@gmail.com>
2025-03-25 23:09:08 +0700

    style: Format my-counter.service.ts

--- sample-app/src/app/services/my-counter.service.ts
+++ sample-app/src/app/services/my-counter.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyCounterService {
-
-  constructor() { }
+  constructor() {}
 
   private counterSource = new Subject<number>();
-  public counter$ = this.counterSource.asObservable()
+  public counter$ = this.counterSource.asObservable();
 
-  inc(val: number) { this.counterSource.next(val + 1) }
-  dec(val: number) { this.counterSource.next(val - 1) }
+  inc(val: number) {
+    this.counterSource.next(val + 1);
+  }
+  dec(val: number) {
+    this.counterSource.next(val - 1);
+  }
 }
