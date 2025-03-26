0035bf67024cf3bfa80efe2974c16b02fb4936cd
MgKyaw <mgkyaw20112@gmail.com>
2025-03-26 22:41:18 +0700

    feat: Modify child component ts

--- nestedApplication/src/app/parent/child/child.component.ts
+++ nestedApplication/src/app/parent/child/child.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-child',
+  standalone: true,
   imports: [],
   templateUrl: './child.component.html',
-  styleUrl: './child.component.css'
+  styleUrl: './child.component.css',
+  styles: 'p { border: 1px solid blue; border-radius: 25px; padding: 16px 32px; }'
 })
 export class ChildComponent {
 
