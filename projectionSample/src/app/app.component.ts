03a214faa11ed5226cee59aa1773d6d775be65a1
MgKyaw <mgkyaw20112@gmail.com>
2025-03-27 21:32:15 +0700

    feat: Import new component

--- projectionSample/src/app/app.component.ts
+++ projectionSample/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContentProjectionSampleComponent } from './content-projection-sample/content-projection-sample.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentProjectionSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
