a09530bfd257b0e4985ee68e239a904cbdcad233
MgKyaw <mgkyaw20112@gmail.com>
2025-03-27 21:43:22 +0700

    feat: Generate directive

---
+++ projectionSample/src/app/directives/greet-content.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GreetContentDirective } from './greet-content.directive';
+
+describe('GreetContentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GreetContentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
