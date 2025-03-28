acdf1f2a23a218ba43c444d5be4c5efa16779718
MgKyaw <mgkyaw20112@gmail.com>
2025-03-29 00:04:02 +0700

    feat: Generate new component check-bio

---
+++ dynamicComponents/src/app/check-bio/check-bio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-bio',
+  imports: [],
+  templateUrl: './check-bio.component.html',
+  styleUrl: './check-bio.component.css'
+})
+export class CheckBioComponent {
+
+}
