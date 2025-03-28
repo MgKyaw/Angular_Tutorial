7e505252cebc34abdd0d87d4d2973d65f7572457
MgKyaw <mgkyaw20112@gmail.com>
2025-03-28 21:25:47 +0700

    feat: Import GreetContentDirective component

--- projectionSample/src/app/app.component.ts
+++ projectionSample/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentProjectionSampleComponent } from './content-projection-sample/content-projection-sample.component';
+import { GreetContentDirective } from './directives/greet-content.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContentProjectionSampleComponent],
+  imports: [RouterOutlet, ContentProjectionSampleComponent, GreetContentDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
