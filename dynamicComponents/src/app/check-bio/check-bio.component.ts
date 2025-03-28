ec51920d67f31fad9fce4c028d542f98081e70ba
MgKyaw <mgkyaw20112@gmail.com>
2025-03-29 00:07:23 +0700

    feat: Create a method getBioComponent

--- dynamicComponents/src/app/check-bio/check-bio.component.ts
+++ dynamicComponents/src/app/check-bio/check-bio.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AdminBioComponent } from '../admin-bio/admin-bio.component';
+import { StandardBioComponent } from '../standard-bio/standard-bio.component';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-check-bio',
-  imports: [],
+  standalone: true,
+  imports: [NgComponentOutlet],
   templateUrl: './check-bio.component.html',
   styleUrl: './check-bio.component.css'
 })
 export class CheckBioComponent {
-
+  @Input() user!: {name: string, isAdmin: boolean};
+  getBioComponent() { 
+    console.log(this.user.isAdmin)
+    return this.user.isAdmin ? AdminBioComponent : StandardBioComponent;
+  }
 }
