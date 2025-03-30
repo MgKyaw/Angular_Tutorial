963118109f62a5e2f0d2b82adaf2e822c244bfeb
MgKyaw <mgkyaw20112@gmail.com>
2025-03-30 19:30:51 +0700

    feat: Generate DynTable c

---
+++ dynamicComponents/src/app/dyn-table/dyn-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dyn-table',
+  imports: [],
+  templateUrl: './dyn-table.component.html',
+  styleUrl: './dyn-table.component.css'
+})
+export class DynTableComponent {
+
+}
