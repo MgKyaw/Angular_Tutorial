7125c081a8e3eed965937bcce24e0af57fbc6f24
MgKyaw <mgkyaw20112@gmail.com>
2025-03-30 19:37:47 +0700

    feat: Implement DynList interface

--- dynamicComponents/src/app/dyn-list/dyn-list.component.ts
+++ dynamicComponents/src/app/dyn-list/dyn-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { DynData } from '../dyn-data'
 
 @Component({
-  selector: 'app-dyn-list',
-  imports: [],
-  templateUrl: './dyn-list.component.html',
-  styleUrl: './dyn-list.component.css'
+   selector: 'app-dyn-list',
+   templateUrl: './dyn-list.component.html',
+   styleUrls: ['./dyn-list.component.css']
 })
-export class DynListComponent {
-
+export class DynListComponent implements DynData {
+   data: any[] = []
 }
