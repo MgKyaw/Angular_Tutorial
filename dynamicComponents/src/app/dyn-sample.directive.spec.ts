1741aa2ed64c2f54c7f54f2e22f934e9d40de5f6
MgKyaw <mgkyaw20112@gmail.com>
2025-03-30 19:30:04 +0700

    feat: Generate DynSample directive

---
+++ dynamicComponents/src/app/dyn-sample.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DynSampleDirective } from './dyn-sample.directive';
+
+describe('DynSampleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DynSampleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
