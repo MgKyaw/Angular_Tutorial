769a2e811851109253b04b38749a268730fc2936
MgKyaw <mgkyaw20112@gmail.com>
2025-03-31 19:52:45 +0700

    feat: Import CommonModule

--- dynamicComponents/src/app/dyn-list/dyn-list.component.ts
+++ dynamicComponents/src/app/dyn-list/dyn-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { DynData } from '../dyn-data'
+import { DynData } from '../dyn-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
    selector: 'app-dyn-list',
+   imports: [CommonModule],
    templateUrl: './dyn-list.component.html',
    styleUrls: ['./dyn-list.component.css']
 })
