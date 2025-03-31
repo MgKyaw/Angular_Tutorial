3c32d5d519bc52c5f755292b899864f2619cd1b8
MgKyaw <mgkyaw20112@gmail.com>
2025-03-31 19:50:05 +0700

    feat: Import necessary components and implement OnInit

--- dynamicComponents/src/app/dyn-host/dyn-host.component.ts
+++ dynamicComponents/src/app/dyn-host/dyn-host.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, OnInit, ViewChild, Input } from '@angular/core';
+import { DynData } from '../dyn-data'
+import { DynSampleDirective } from '../dyn-sample.directive'
+import { DynListComponent } from '../dyn-list/dyn-list.component'
+import { DynTableComponent } from '../dyn-table/dyn-table.component'
 
 @Component({
-  selector: 'app-dyn-host',
-  imports: [],
-  templateUrl: './dyn-host.component.html',
-  styleUrl: './dyn-host.component.css'
+   selector: 'app-dyn-host',
+   templateUrl: './dyn-host.component.html',
+   styleUrls: ['./dyn-host.component.css']
 })
-export class DynHostComponent {
-
+export class DynHostComponent implements OnInit {
+   @Input() format: string = 'table'
+   
+   private data = [
+   {
+      'name': 'John',
+      'role': "Manager"
+   },
+   {
+      'name': 'Peter',
+      'role': "Marketing Intern"
+   },
+   {
+      'name': 'Mary',
+      'role': "Technical Intern"
+   },
+   {
+      'name': 'Jack',
+      'role': "Sales Manager"
+   },
+   {
+      'name': 'Jessica',
+      'role': "Delivery Head"
+   },
+   ]
+   
+   @ViewChild(DynSampleDirective, {static: true}) host!: DynSampleDirective;
+   
+   ngOnInit() {
+      const viewContainerRef = this.host.viewContainerRef;
+      viewContainerRef.clear()
+      
+      if(this.format == 'table') {
+         const compRef = viewContainerRef.createComponent<DynData>(DynTableComponent);
+         compRef.instance.data = this.data;
+      } else {
+         const compRef = viewContainerRef.createComponent<DynData>(DynListComponent);
+         compRef.instance.data = this.data;
+      }
+   }
 }
