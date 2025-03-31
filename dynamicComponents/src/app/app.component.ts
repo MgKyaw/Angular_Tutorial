cb20b86fc611a817cf1c5e4e1d1a7eac9aafa4e3
MgKyaw <mgkyaw20112@gmail.com>
2025-03-31 19:53:21 +0700

    feat: Import required components

--- dynamicComponents/src/app/app.component.ts
+++ dynamicComponents/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CheckBioComponent } from './check-bio/check-bio.component';
 import { CommonModule } from '@angular/common';
+import { DynHostComponent } from './dyn-host/dyn-host.component';
+import { DynListComponent } from './dyn-list/dyn-list.component';
+import { DynTableComponent } from './dyn-table/dyn-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckBioComponent, CommonModule],
+  imports: [RouterOutlet, CheckBioComponent, CommonModule, DynHostComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
