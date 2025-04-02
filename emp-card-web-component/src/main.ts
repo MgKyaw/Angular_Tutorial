1a87d730f7643bed37f04f47e98957cc5a3fa3e7
MgKyaw <mgkyaw20112@gmail.com>
2025-04-02 21:48:32 +0700

    feat: Import required components

---
+++ emp-card-web-component/src/main.ts
@@ -0,0 +1,4 @@
+import { createApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { EmpCardComponent } from './app/emp-card/emp-card.component'
+
