fd254d66d60ae937252ebccb0840df30d3c0ea60
MgKyaw <mgkyaw20112@gmail.com>
2025-04-02 21:50:16 +0700

    feat: Create custom element

--- emp-card-web-component/src/main.ts
+++ emp-card-web-component/src/main.ts
@@ -3,5 +3,8 @@ import { createCustomElement } from '@angular/elements';
 import { EmpCardComponent } from './app/emp-card/emp-card.component'
 
 createApplication({ providers: [] }).then((appRef) => {
-  // ...
+  const empCard = createCustomElement(
+     EmpCardComponent,
+     { injector: appRef.injector }
+  );
 });
