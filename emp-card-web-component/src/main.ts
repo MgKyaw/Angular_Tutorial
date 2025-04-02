9aa31f0d789c6353eb7eac0ceb540cc227f0a853
MgKyaw <mgkyaw20112@gmail.com>
2025-04-02 21:49:05 +0700

    feat: Create application

--- emp-card-web-component/src/main.ts
+++ emp-card-web-component/src/main.ts
@@ -2,3 +2,6 @@ import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { EmpCardComponent } from './app/emp-card/emp-card.component'
 
+createApplication({ providers: [] }).then((appRef) => {
+  // ...
+});
