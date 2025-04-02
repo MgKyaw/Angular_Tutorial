061ae8f6b5d5bd85af248e6cdb602d628e045a78
MgKyaw <mgkyaw20112@gmail.com>
2025-04-02 21:40:32 +0700

    feat: Add encapsulation option

--- emp-card-web-component/src/app/emp-card/emp-card.component.ts
+++ emp-card-web-component/src/app/emp-card/emp-card.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-emp-card',
   imports: [],
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './emp-card.component.html',
   styleUrl: './emp-card.component.css'
 })
