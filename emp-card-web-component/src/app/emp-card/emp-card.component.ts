ca2c7b6da6829dff6c6ad9a22214ac4e04f60973
MgKyaw <mgkyaw20112@gmail.com>
2025-04-03 22:10:52 +0700

    feat: Make empCardComponent standlone

--- emp-card-web-component/src/app/emp-card/emp-card.component.ts
+++ emp-card-web-component/src/app/emp-card/emp-card.component.ts
@@ -2,10 +2,10 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-emp-card',
-  imports: [],
+  standalone: true,
+  // imports: [],
   templateUrl: './emp-card.component.html',
   styleUrl: './emp-card.component.css',
-  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class EmpCardComponent {
   @Input() name: string = '';
