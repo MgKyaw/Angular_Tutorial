386ae852cc7dd6bfc27af58bae159f63bfd7b235
MgKyaw <mgkyaw20112@gmail.com>
2025-04-03 22:11:23 +0700

    feat: Comment the existing code from main.server.ts

--- emp-card-web-component/src/main.server.ts
+++ emp-card-web-component/src/main.server.ts
@@ -1,7 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+// const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-export default bootstrap;
+// export default bootstrap;
+
+// import { createApplication } from '@angular/platform-server';
+// import { createCustomElement } from '@angular/elements';
+// import { EmpCardComponent } from './app/emp-card/emp-card.component';
+
+// export default function mainServer() {
+//   return createApplication({ providers: [] }).then((appRef) => {
+//     const empCard = createCustomElement(EmpCardComponent, { injector: appRef.injector });
+//     customElements.define('emp-card', empCard);
+//   });
+// }
