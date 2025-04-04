77304e513fb2e0f8b06f503b22ed9b48e889c647
MgKyaw <mgkyaw20112@gmail.com>
2025-04-04 20:58:27 +0700

    feat: Generate new component emp-card

---
+++ emp-card-web-component2/src/app/emp-card/emp-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-card',
+  imports: [],
+  templateUrl: './emp-card.component.html',
+  styleUrl: './emp-card.component.css'
+})
+export class EmpCardComponent {
+
+}
