50dd0cf68d990457f59fd43bde6973dc515af96c
MgKyaw <mgkyaw20112@gmail.com>
2025-04-04 21:25:44 +0700

    feat: Add create application

--- emp-card-web-component2/src/main.ts
+++ emp-card-web-component2/src/main.ts
@@ -1,3 +1,10 @@
 import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { EmpCardComponent } from './app/emp-card/emp-card.component'
+
+createApplication({ providers: [] }).then((appRef) => {
+  const empCard = createCustomElement(
+     EmpCardComponent,
+     { injector: appRef.injector }
+  );
+});
