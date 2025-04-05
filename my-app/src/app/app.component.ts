34c592cc418c4eb7840aa34d5c26a4ecce5fbd31
MgKyaw <mgkyaw20112@gmail.com>
2025-04-05 20:49:22 +0700

    feat: Import BlockWithShowHideComponent

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockWithShowHideComponent } from './block-with-show-hide/block-with-show-hide.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlockWithShowHideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
