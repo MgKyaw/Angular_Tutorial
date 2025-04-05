d62b9171885851db9b24bfbe41ddae40ed21f07b
MgKyaw <mgkyaw20112@gmail.com>
2025-04-05 21:08:49 +0700

    feat: Import MyTemplateRefVarSampleComponent

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlockWithShowHideComponent } from './block-with-show-hide/block-with-show-hide.component';
+import { MyTemplateRefVarSampleComponent } from './my-template-ref-var-sample/my-template-ref-var-sample.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BlockWithShowHideComponent],
+  imports: [RouterOutlet, BlockWithShowHideComponent, MyTemplateRefVarSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
