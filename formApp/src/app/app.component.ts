1b255eb432d2d2ef1a2c33e980cac2435ef946a1
MgKyaw <mgkyaw20112@gmail.com>
2025-04-10 20:29:48 +0700

    feat: Define a variable user

--- formApp/src/app/app.component.ts
+++ formApp/src/app/app.component.ts
@@ -9,5 +9,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'formApp';
+  title = 'Two Way Binding';
+  user: string = '';
 }
