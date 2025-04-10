49d6b48b4b0b49e7b0a5f691fc69067b57def834
MgKyaw <mgkyaw20112@gmail.com>
2025-04-10 20:09:36 +0700

    feat: Add an object btnStyle

--- style-binding/src/app/register-form/register-form.component.ts
+++ style-binding/src/app/register-form/register-form.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
-
+  btnStyle: Record<string, string> = {
+    'backgroundColor': 'purple',
+    'color': 'white',
+    'padding': '15px 20px',
+    'margin': '10px 0',
+    'border': 'none',
+    'cursor': 'pointer',
+    'width': '100%'
+ }
 }
