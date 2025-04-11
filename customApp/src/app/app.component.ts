06b513e47eddbb38d69d8b78a83251ad373c118f
MgKyaw <mgkyaw20112@gmail.com>
2025-04-11 12:19:15 +0700

    feat: Import modules for two way binding

--- customApp/src/app/app.component.ts
+++ customApp/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponentComponent } from './child-component/child-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, ChildComponentComponent],
+  template: `<h3>{{ title }}</h3>
+             <app-child-component [(val)]="user"></app-child-component> 
+             <p>Hello {{ user }}!</p>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'customApp';
+  title = 'Two Way Binding Example';
+  user: string = "";
 }
