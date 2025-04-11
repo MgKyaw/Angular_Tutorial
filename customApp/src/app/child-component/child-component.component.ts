9b19ad731d7a0f7f8931bb270f5b93328ea2b455
MgKyaw <mgkyaw20112@gmail.com>
2025-04-11 12:17:37 +0700

    feat: Apply two way binding

--- customApp/src/app/child-component/child-component.component.ts
+++ customApp/src/app/child-component/child-component.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-child-component',
-  imports: [],
-  templateUrl: './child-component.component.html',
-  styleUrl: './child-component.component.css'
+  standalone: true,
+  imports: [FormsModule],
+  template: '<input [value]="val" (input)="onValChange($event)" />',
+  styleUrls: ['./child-component.component.css']
 })
 export class ChildComponentComponent {
+  @Input() val: string = '';
+  @Output() valChange: EventEmitter<string> = new EventEmitter<string>();
 
+  onValChange(event: Event) {
+    // Cast the event target to HTMLInputElement and access the value
+    const input = event.target as HTMLInputElement;
+    if (input) {
+      this.valChange.emit(input.value);
+    }
+  }
 }
