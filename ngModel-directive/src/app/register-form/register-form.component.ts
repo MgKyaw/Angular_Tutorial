3e6a09087a10213b3f54c4f753463b7bc2c11868
MgKyaw <mgkyaw20112@gmail.com>
2025-04-12 10:28:10 +0700

    feat: Create var and method in component

--- ngModel-directive/src/app/register-form/register-form.component.ts
+++ ngModel-directive/src/app/register-form/register-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  user: any = {
+    username: '',
+    password: '',
+    confirmPassword: ''
+ }
 
+ showInfo(e: Event) {
+  e.preventDefault();
+  let info: string = '';
+  info += 'Username = ' + this.user.username;
+  info += '\nPassword = ' + this.user.password;
+  info += '\nConfirm password = ' + this.user.confirmPassword;
+  alert(info)
+}
 }
