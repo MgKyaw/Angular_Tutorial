20a11e2cc722f4d20a394a2c0917afc6c3ec1bf9
MgKyaw <mgkyaw20112@gmail.com>
2025-04-12 10:42:46 +0700

    feat: Import CommonModule and create showData var

--- structural-directives/src/app/app.component.ts
+++ structural-directives/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'structural-directives';
+  showData = false; 
+  show() { 
+    this.showData = !this.showData; 
+  }
 }
