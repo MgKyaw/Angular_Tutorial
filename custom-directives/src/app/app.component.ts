7ecea10cb175f5ed0815d875603cd6e79df5bb2d
MgKyaw <mgkyaw20112@gmail.com>
2025-04-15 12:47:52 +0700

    feat: Import CustomstyleDirective

--- custom-directives/src/app/app.component.ts
+++ custom-directives/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomstyleDirective } from './customstyle.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomstyleDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
