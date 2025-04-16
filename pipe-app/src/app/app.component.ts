762742bab47f8b355538d438bbd30b85f77140b7
MgKyaw <mgkyaw20112@gmail.com>
2025-04-16 10:04:29 +0700

    feat: Import DatePipe

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
