89ee4df1d7b0e4c7b958327308d8c05ebf370e36
MgKyaw <mgkyaw20112@gmail.com>
2025-04-17 11:36:43 +0700

    feat: Import LowerCasePipe and create a string array

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe],
+  imports: [RouterOutlet, DatePipe, LowerCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pipe-app';
   presentDate = new Date();
+  chapterList: string[] = ["Binding", "Pipe", "Services"];
 }
