7342a5f34c31c243d29ac61cdbbbe2d17f944ead
MgKyaw <mgkyaw20112@gmail.com>
2025-04-17 11:54:53 +0700

    feat: Import SlicePipe

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { LowerCasePipe, UpperCasePipe } from '@angular/common';
+import { LowerCasePipe, UpperCasePipe, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe],
+  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
