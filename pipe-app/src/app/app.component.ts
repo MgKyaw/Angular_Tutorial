3c88b38ba20ef719f34402f471994941a7ec2915
MgKyaw <mgkyaw20112@gmail.com>
2025-04-18 21:34:08 +0700

    feat: Import CurrencyPipe

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { LowerCasePipe, UpperCasePipe, SlicePipe } from '@angular/common';
+import { LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe],
+  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
