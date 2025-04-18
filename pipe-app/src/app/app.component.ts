3f4a0c6a9acba63cf93a59c1d41747b92b04e61c
MgKyaw <mgkyaw20112@gmail.com>
2025-04-18 21:36:10 +0700

    feat: Import PercentagePipe

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe } from '@angular/common';
+import { LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe],
+  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe, PercentPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
