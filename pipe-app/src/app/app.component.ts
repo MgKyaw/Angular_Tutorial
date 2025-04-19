efba9cda94243c2bf596ac8041fa4624d80507db
MgKyaw <mgkyaw20112@gmail.com>
2025-04-19 23:34:46 +0700

    feat: Import DigitcountPipe

--- pipe-app/src/app/app.component.ts
+++ pipe-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe, PercentPipe } from '@angular/common';
+import { DigitcountPipe } from './digitcount.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe, PercentPipe],
+  imports: [RouterOutlet, DatePipe, LowerCasePipe, UpperCasePipe, SlicePipe, CurrencyPipe, PercentPipe, DigitcountPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
