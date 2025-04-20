703dcc410e4994ebf4a4bbc44f6a0d76c12b5c87
MgKyaw <mgkyaw20112@gmail.com>
2025-04-20 17:07:28 +0700

    feat: Import FormsModule and Update content

--- template-form-app/src/app/app.component.ts
+++ template-form-app/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'template-form-app';
+  username: string = ""; 
+  submitted: boolean = false; 
+  ngOnInit() { } 
+  onClickSubmit(result: { username: string; }) { 
+    this.username = result.username; 
+    this.submitted = true; 
+  }
 }
