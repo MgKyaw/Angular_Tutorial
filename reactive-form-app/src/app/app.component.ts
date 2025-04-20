df67b2ee07a5a65b4b67184e510aeb479b1363da
MgKyaw <mgkyaw20112@gmail.com>
2025-04-20 17:31:03 +0700

    feat: Implement reactive form in component

--- reactive-form-app/src/app/app.component.ts
+++ reactive-form-app/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'reactive-form-app';
+export class AppComponent implements OnInit {
+  title = 'form-app';
+  userName: string = ""; 
+  formdata: FormGroup = new FormGroup({});
+  ngOnInit() { 
+    this.formdata = new FormGroup({ 
+      userName: new FormControl("") 
+    }); 
+  } 
+  onClickSubmit(data: { userName: string }) { 
+    this.userName = data.userName; 
+  }
 }
