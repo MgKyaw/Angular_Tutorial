50d16684a27188ed0fd05621deec7eb1d631d3a3
MgKyaw <mgkyaw20112@gmail.com>
2025-04-21 17:28:23 +0700

    feat: Import modules and implement validation for reactive form

--- form-validation/src/app/app.component.ts
+++ form-validation/src/app/app.component.ts
@@ -1,23 +1,54 @@
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { RouterOutlet } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//    selector: 'app-root',
+//    standalone: true,
+//    imports: [RouterOutlet, FormsModule, CommonModule],
+//    templateUrl: './app.component.html',
+//    styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//    user = {
+//       email: '',
+//       password: ''
+//    };
+//    onSubmit(form: any) {
+//       if (form.valid) {
+//          console.log('Form Submitted!', this.user);
+//       }
+//    }
+// }
+
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
    selector: 'app-root',
    standalone: true,
-   imports: [RouterOutlet, FormsModule, CommonModule],
+   imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
    templateUrl: './app.component.html',
    styleUrl: './app.component.css'
 })
 export class AppComponent {
-   user = {
-      email: '',
-      password: ''
-   };
-   onSubmit(form: any) {
-      if (form.valid) {
-         console.log('Form Submitted!', this.user);
+   loginForm: FormGroup;
+   
+   constructor() {
+      this.loginForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      });
+   }
+
+   onSubmit() {
+      if (this.loginForm.valid) {
+         console.log('Form Submitted!', this.loginForm.value);
+      } else {
+         console.log('Form is not valid');
       }
    }
 }
