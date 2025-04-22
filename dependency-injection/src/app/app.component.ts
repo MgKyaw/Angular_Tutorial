b28d34e7504ca58f99cb3abc96f0b49df982fc3b
MgKyaw <mgkyaw20112@gmail.com>
2025-04-22 23:10:53 +0700

    feat: Import service and implement DI

--- dependency-injection/src/app/app.component.ts
+++ dependency-injection/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataproviderService } from './dataprovider.service';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+   selector: 'app-root',
+   standalone: true,
+   imports: [RouterOutlet],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dependency-injection';
+   color: string[];
+   
+   constructor(private colorService: DataproviderService) {
+      this.color = this.colorService.getData(); 
+   }
 }
