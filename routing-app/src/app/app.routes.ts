dd226ac16a6f75411079539516de6736b7e6656f
MgKyaw <mgkyaw20112@gmail.com>
2025-04-26 00:13:45 +0700

    feat: Use wildcard ** for pagenotfound

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ViewItemComponent } from './view-item/view-item.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'view/:id', component: ViewItemComponent}
+    {path: 'view/:id', component: ViewItemComponent},
+    {path: '**', component: PagenotfoundComponent}
 ];
