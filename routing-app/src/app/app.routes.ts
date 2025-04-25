c5ec32fa0a2a44d4a5414c11e80a55125580f8fa
MgKyaw <mgkyaw20112@gmail.com>
2025-04-25 10:58:34 +0700

    feat: Add route with route parameter

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ViewItemComponent } from './view-item/view-item.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'view/:id', component: ViewItemComponent}
 ];
