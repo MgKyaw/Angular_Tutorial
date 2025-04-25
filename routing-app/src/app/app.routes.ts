fdc3dc4101168e7e88c83a42aa419c0d9b87ffca
MgKyaw <mgkyaw20112@gmail.com>
2025-04-26 00:18:19 +0700

    feat: Render parent and child menus

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ViewItemComponent } from './view-item/view-item.component';
+import { EditItemComponent } from './edit-item/edit-item.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'view/:id', component: ViewItemComponent},
+    {path: 'item', children:[
+        {path: 'view/:id', component: ViewItemComponent},
+        {path: 'edit/:id', component: EditItemComponent},
+    ]},
     {path: '**', component: PagenotfoundComponent}
 ];
