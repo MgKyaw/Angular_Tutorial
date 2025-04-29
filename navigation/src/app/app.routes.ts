05dd1993056def71a966609ca9d35bc9adf834a1
MgKyaw <mgkyaw20112@gmail.com>
2025-04-29 09:14:59 +0700

    feat: Add routes

--- navigation/src/app/app.routes.ts
+++ navigation/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'product', component: ProductComponent},
+    {path: 'inventory', component: InventoryComponent}
+];
