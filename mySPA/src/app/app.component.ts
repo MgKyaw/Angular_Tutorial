ef0f12fafafb4c14a602b8529639b4aef1de36a7
MgKyaw <mgkyaw20112@gmail.com>
2025-04-30 09:50:19 +0700

    feat: Import CommonModule

--- mySPA/src/app/app.component.ts
+++ mySPA/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ContactComponent],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
