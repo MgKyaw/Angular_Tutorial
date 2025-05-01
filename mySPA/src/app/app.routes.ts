a9a506f3aa97ac7d151d9d9520bfa81281241997
MgKyaw <mgkyaw20112@gmail.com>
2025-05-01 20:39:39 +0700

    feat: Add routes

--- mySPA/src/app/app.routes.ts
+++ mySPA/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'contact', component: ContactComponent}
 ];
