7a3b949354503c03e28496880d90c3df1597d678
MgKyaw <mgkyaw20112@gmail.com>
2025-05-05 21:52:05 +0700

    feat: Define custom route matcher

--- angular-custom-route/src/app/app.routes.ts
+++ angular-custom-route/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlSegment } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        matcher: (url) =>{
+            if(url.length === 1 && url[0].path.match(/^@[\w]+$/gm)){
+                return {
+				consumed: url, 
+				posParams: {
+				dashboard: new UrlSegment(url[0].path.slice(1), {})}};
+            }
+            else{
+                return null;
+            }
+        },
+        component: DashboardComponent
+    }
+];
