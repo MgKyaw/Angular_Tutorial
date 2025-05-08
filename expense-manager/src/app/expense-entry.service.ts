839c43125c6e853d5c469432f9d4a85b08ec167a
MgKyaw <mgkyaw20112@gmail.com>
2025-05-08 21:00:02 +0700

    feat: Add httpErrorHandler method in ExpenseEntryService

--- expense-manager/src/app/expense-entry.service.ts
+++ expense-manager/src/app/expense-entry.service.ts
@@ -14,4 +14,16 @@ export class ExpenseEntryService {
  };
 
   constructor(private httpClient : HttpClient) { }
+
+  private httpErrorHandler (error: HttpErrorResponse) {
+     if (error.error instanceof ErrorEvent) {
+        console.error("A client side error occurs. The error message is " + error.message);
+     } else {
+        console.error(
+           "An error happened in server. The HTTP status code is "  + 
+       error.status + " and the error returned is " + error.message);
+     }
+  
+     return throwError("Error occurred. Pleas try again");
+  }
 }
