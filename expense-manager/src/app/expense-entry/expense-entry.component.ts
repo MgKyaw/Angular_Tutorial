dc431a55d2ac0756f4850f565e05674720be2cc0
MgKyaw <mgkyaw20112@gmail.com>
2025-05-09 11:44:03 +0700

    feat: Import and inject ExpenseEntryService

--- expense-manager/src/app/expense-entry/expense-entry.component.ts
+++ expense-manager/src/app/expense-entry/expense-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseEntryService } from '../expense-entry.service';
 
 @Component({
   selector: 'app-expense-entry',
@@ -9,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseEntryComponent {
   title: any;
-  constructor() {}
+  constructor(private restService : ExpenseEntryService ) { }
   ngOnInit(): void {
     this.title = "Expense Entry"
   }
