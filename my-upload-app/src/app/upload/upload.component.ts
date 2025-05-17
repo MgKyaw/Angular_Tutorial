e04d74cefe8d714435292dc5be35bf7988961b01
MgKyaw <mgkyaw20112@gmail.com>
2025-05-17 11:52:57 +0700

    feat: Add getEventMessage method

--- my-upload-app/src/app/upload/upload.component.ts
+++ my-upload-app/src/app/upload/upload.component.ts
@@ -3,15 +3,42 @@ import { HttpClient, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 
 @Component({
-   selector: 'app-upload',
-   templateUrl: './upload.component.html',
-   styleUrls: ['./upload.component.css']
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
-   constructor(private http: HttpClient) { }
+  file?: File | null = null;
+  message: String | null = null;
 
-   file?: File | null = null;
-   message : String | null = null; 
+  constructor(private http: HttpClient) {}
 
+  private getEventMessage(event: HttpEvent<any>, file?: File) {
+    let message: String | null = null;
+    switch (event.type) {
+      case HttpEventType.Sent:
+        message = `Uploading file "${file?.name}" of size ${file?.size}.`;
+        console.log(message);
+        return message;
 
+      case HttpEventType.UploadProgress:
+        // Compute and show the % done:
+        const percentDone = event.total
+          ? Math.round((100 * event.loaded) / event.total)
+          : 0;
+        message = `File "${file?.name}" is ${percentDone}% uploaded.`;
+        console.log(message);
+        return message;
+
+      case HttpEventType.Response:
+        message = `File "${file?.name}" was completely uploaded!`;
+        console.log(message);
+        return message;
+
+      default:
+        message = `File "${file?.name}" surprising upload event: ${event.type}.`;
+        console.log(message);
+        return message;
+    }
+  }
 }
