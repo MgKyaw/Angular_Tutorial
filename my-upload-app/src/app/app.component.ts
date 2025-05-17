0b34bad0e54e054fca0eb4d54ca6ad752954d7a9
MgKyaw <mgkyaw20112@gmail.com>
2025-05-17 11:23:52 +0700

    feat: Import UploadComponent

--- my-upload-app/src/app/app.component.ts
+++ my-upload-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { UploadComponent } from './upload/upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
