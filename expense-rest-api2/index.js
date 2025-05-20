e65837f0833bf3df08b88dd1a716320a77f3e53b
MgKyaw <mgkyaw20112@gmail.com>
2025-05-20 23:29:07 +0700

    feat: Add index.js

---
+++ expense-rest-api2/index.js
@@ -0,0 +1,48 @@
+var express = require("express")
+var cors = require('cors')
+var db = require("./sqlitedb.js")
+
+var app = express()
+app.use(cors());
+
+var bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var HTTP_PORT = 8000
+app.listen(HTTP_PORT, () => {
+   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+});
+
+app.get("/", (req, res, next) => {
+   res.json({ "message": "Ok" })
+});
+
+app.get("/api/expense", (req, res, next) => {
+   var sql = "select * from expense"
+   var params = []
+   db.all(sql, params, (err, rows) => {
+      if (err) {
+         res.status(400).json({ "error": err.message });
+         return;
+      }
+      res.json(rows)
+   });
+   
+});
+
+app.get("/api/expense/:id", (req, res, next) => {
+   var sql = "select * from expense where id = ?"
+   var params = [req.params.id]
+   db.get(sql, params, (err, row) => {
+      if (err) {
+         res.status(400).json({ "error": err.message });
+         return;
+      }
+      res.json(row)
+   });
+});
+
+app.use(function (req, res) {
+   res.status(404);
+});
