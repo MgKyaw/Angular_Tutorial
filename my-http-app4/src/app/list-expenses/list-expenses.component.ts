9ac7a054a33968c19cf20f17bcbdad7ac3cb16b8
MgKyaw <mgkyaw20112@gmail.com>
2025-05-29 21:34:39 +0700

    feat: Inject HttpClient

--- my-http-app4/src/app/list-expenses/list-expenses.component.ts
+++ my-http-app4/src/app/list-expenses/list-expenses.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-list-expenses',
-  imports: [],
-  templateUrl: './list-expenses.component.html',
-  styleUrl: './list-expenses.component.css'
+   selector: 'app-list-expenses',
+   templateUrl: './list-expenses.component.html',
+   styleUrls: ['./list-expenses.component.css']	
 })
 export class ListExpensesComponent {
 
+   constructor(private http: HttpClient) { }
 }
