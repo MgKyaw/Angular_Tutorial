2d5847772039592025b84a3a64f714b80f58d9cd
MgKyaw <mgkyaw20112@gmail.com>
2025-05-30 22:35:36 +0700

    feat: Call post method

--- my-http-app4/src/app/list-expenses/list-expenses.component.ts
+++ my-http-app4/src/app/list-expenses/list-expenses.component.ts
@@ -16,12 +16,20 @@ export class ListExpensesComponent implements OnInit {
       var spend_date = new Date();
       spend_date.setDate(spend_date.getDate() - 1);
       this.newexpense = {
-         'id': 0,
          'item': 'new item ' + Math.floor(Math.random() * 10),
          'amount': Math.floor(Math.random() * 100),
          'category': 'Food',
          'location': 'KFC',
          'spendOn': spend_date
-      }   
+      }
+      
+      this.http.post<Expense>('http://localhost:8000/api/expense/1', this.newexpense,{
+         'observe': 'body',
+         'responseType': 'json'
+      })
+      .subscribe( data => {
+         this.newexpense = data as Expense;
+         console.log(data)
+      });      
    }
 }
