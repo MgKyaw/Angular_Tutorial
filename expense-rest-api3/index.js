c84c61bf422e2bac7ede4c4ba227fb0e933967bf
MgKyaw <mgkyaw20112@gmail.com>
2025-06-01 22:01:56 +0700

    feat: Update index.js for PUT method

--- expense-rest-api3/index.js
+++ expense-rest-api3/index.js
@@ -14,11 +14,11 @@ app.listen(HTTP_PORT, () => {
    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-app.POST("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
    res.json({ "message": "Ok" })
 });
 
-app.POST("/api/expense", (req, res, next) => {
+app.get("/api/expense", (req, res, next) => {
    var sql = "select * from expense"
    var params = []
    db.all(sql, params, (err, rows) => {
@@ -28,20 +28,79 @@ app.POST("/api/expense", (req, res, next) => {
       }
       res.json(rows)
    });
-   
+
 });
 
-app.POST("/api/expense/:id", (req, res, next) => {
+app.get("/api/expense/:id", (req, res, next) => {
    var sql = "select * from expense where id = ?"
    var params = [req.params.id]
-   db.POST(sql, params, (err, row) => {
+   db.get(sql, params, (err, row) => {
       if (err) {
          res.status(400).json({ "error": err.message });
          return;
       }
-      res.json(row)
+   res.json(row)
    });
 });
+
+app.put("/api/expense/:id", (req, res, next) => {
+   if (req.params.id == null) {
+      res.status(400).json({ "error": "Resource (Expense) Id is not send." })
+      return
+   }
+   
+   var data = {
+      id: req.params.id,
+      item: req.body.item,
+      amount: req.body.amount,
+      category: req.body.category,
+      location: req.body.location,
+      spendOn: req.body.spendOn
+   }
+   
+   var sql = 'SELECT count(*) AS cnt FROM expense WHERE id = ?'
+   var params = [data.id]
+   db.get(sql, params, function (err, result) {
+   if (err) {
+      res.status(400).json({ "error": err.message })
+      return;
+   }
+   
+   if (result.cnt == 0) {
+      var sql = 'INSERT INTO expense (id, item, amount, category, location, spendOn, createdOn) VALUES (?, ?,?,?,?,?,?)'
+      var params = [data.id, data.item, data.amount, data.category, data.location, data.spendOn, data.createdOn]
+      db.run(sql, params, function (err, result) {
+         if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+         }
+         console.log(result)
+         res.json(data);
+      });
+   } else {
+      db.run(
+         `UPDATE expense SET
+            item = ?, 
+            
+            amount = ?,
+            category = ?, 
+            location = ?, 
+            
+            spendOn = ? 
+            WHERE id = ?`,
+         [data.item, data.amount, data.category, data.location, data.spendOn, data.id],
+         function (err, result) {
+            if (err) {
+               console.log(err);
+               res.status(400).json({ "error": res.message })
+               return;
+            }
+            res.json(data)
+         });
+      }
+   });
+
+})
 
 app.use(function (req, res) {
    res.status(404);
