3c4ccc35de2255c9e0fb8b304246b943e0d897f4
MgKyaw <mgkyaw20112@gmail.com>
2025-06-07 15:14:25 +0700

    feat: Update ListExpenseComponent ts for Angular JSONP tutorial

--- my-http-app4/src/app/list-expenses/list-expenses.component.ts
+++ my-http-app4/src/app/list-expenses/list-expenses.component.ts
@@ -1,59 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import Expense from '../ListExpenses';
 
 @Component({
    selector: 'app-list-expenses',
    templateUrl: './list-expenses.component.html',
    styleUrls: ['./list-expenses.component.css']
 })
-export class ListExpensesComponent implements OnInit {
-   expenses: Expense[] = [];
-   newexpense: Expense | null = null;
+export class ListExpensesComponent {
+
    constructor(private http: HttpClient) { }
-   delete(id? : Number) : void {
-      if (id) {
-         this.http.delete<Expense>('http://localhost:8000/api/expense/' + id,{
-            'observe': 'body',
-            'responseType': 'json'
-         }).subscribe( data => {
-            console.log(data)
-            this.http.get<Expense[]>('http://localhost:8000/api/expense',{
-               'observe': 'body',
-               'responseType': 'json'
-            }).subscribe( data => {
-                this.expenses = data as Expense[]
-                console.log(this.expenses)
-            })
-         });
-      }
-   }   
-   ngOnInit(): void {
-      var spend_date = new Date();
-      spend_date.setDate(spend_date.getDate() - 1);
-      this.newexpense = {
-         'item': 'new item ' + Math.floor(Math.random() * 10),
-         'amount': Math.floor(Math.random() * 100),
-         'category': 'Food',
-         'location': 'KFC',
-         'spendOn': spend_date
-      }
-      this.http.put<Expense>('http://localhost:8000/api/expense/1',
-      this.newexpense,{
-         'observe': 'body',
-         'responseType': 'json'
-      })
-      .subscribe( data => {
-         this.newexpense = data as Expense;
-         console.log(data)
-      });
-      this.http.get<Expense[]>('http://localhost:8000/api/expense',{
-         'observe': 'body',
-         'responseType': 'json'
-      })
-      .subscribe( data => {
-         this.expenses = data as Expense[]
-         console.log(this.expenses)
-      })
-   }
 }
