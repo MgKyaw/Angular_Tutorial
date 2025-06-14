c488a7afa0035c30cbacb89b6771bafdf6b2d0bb
MgKyaw <mgkyaw20112@gmail.com>
2025-06-14 09:58:55 +0700

    feat: Update auth.service.ts

--- my-crud-app/src/app/services/auth.service.ts
+++ my-crud-app/src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,31 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  private baseURL: string = "http://localhost:3000/employees";
+  constructor(private http: HttpClient) { }
+
+  //read employees data ( Read operation)
+  getAllEmployees(){
+    return this.http.get(this.baseURL);
+  }
+
+  //get employee by id (read operation)
+  getEmployeeById(id: any){
+    return this.http.get(this.baseURL + "/" + id);
+  }
+
+  //create or add new employee (Post operation)
+  createEmployee(obj: any){
+    return this.http.post(this.baseURL + "/", obj);
+  }
+
+  //update employee data (Put operation)
+  updateEmployee(id: any, obj: any) {
+    return this.http.put(this.baseURL + '/' + id , obj);
+}
+
+  //delete employee (Delete operation)
+  deleteEmployee(id: any){
+    return this.http.delete(this.baseURL + "/" + id);
+  }
 }
