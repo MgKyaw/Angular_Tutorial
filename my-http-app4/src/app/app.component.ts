7b9c251eaf44887c1372bc7d9c2b5fab9aa00e9a
MgKyaw <mgkyaw20112@gmail.com>
2025-06-14 09:55:09 +0700

    feat: Import HttpClientModule

--- my-http-app4/src/app/app.component.ts
+++ my-http-app4/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+//here....
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  //here....
   imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-http-app';
+  title = 'my-crud-app';
 }
