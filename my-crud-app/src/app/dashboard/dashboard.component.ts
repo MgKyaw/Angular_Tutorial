194a065b672acb4bdbe88e6e45628373265fe134
MgKyaw <mgkyaw20112@gmail.com>
2025-06-15 21:30:24 +0700

    feat: Update dashboard.component.cs

--- my-crud-app/src/app/dashboard/dashboard.component.ts
+++ my-crud-app/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit{
+constructor(private auth: AuthService, private router: ActivatedRoute){}
+allEmployees: any;
+ngOnInit(): void {
+    this.auth.getAllEmployees().subscribe(res=>{
+      this.allEmployees = res;
+      console.log(this.allEmployees);
+    });
+}
 }
