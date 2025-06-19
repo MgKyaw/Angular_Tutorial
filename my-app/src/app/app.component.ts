764f76276dc0a1f62bfdacc0faea729957d91fd6
MgKyaw <mgkyaw20112@gmail.com>
2025-06-19 21:57:26 +0700

    feat: Import AuthModule

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlockWithShowHideComponent } from './block-with-show-hide/block-with-show-hide.component';
 import { MyTemplateRefVarSampleComponent } from './my-template-ref-var-sample/my-template-ref-var-sample.component';
+import { AuthModule } from './auth/auth.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BlockWithShowHideComponent, MyTemplateRefVarSampleComponent],
+  imports: [RouterOutlet, BlockWithShowHideComponent, MyTemplateRefVarSampleComponent, AuthModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
