219555ec97e7e2b4ae4b1cdd7f0fa4fde811ff7e
MgKyaw <mgkyaw20112@gmail.com>
2025-06-21 09:08:03 +0700

    feat: Import the feature module AdminModule

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule],
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
