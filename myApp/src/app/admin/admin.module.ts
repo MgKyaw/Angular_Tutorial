09c81bb72032db1dffe23a914a66d24e899bdf9c
MgKyaw <mgkyaw20112@gmail.com>
2025-06-21 09:04:58 +0700

    feat: Generate admin/Contact component

--- myApp/src/app/admin/admin.module.ts
+++ myApp/src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
