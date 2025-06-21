5aa4ccdf16a91d112b6ae258a908326133722435
MgKyaw <mgkyaw20112@gmail.com>
2025-06-21 09:04:19 +0700

    feat: Generate admin/About

--- myApp/src/app/admin/admin.module.ts
+++ myApp/src/app/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
