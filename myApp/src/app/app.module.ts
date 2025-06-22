18b3a14f1232494b294f7917c664b8129098b6eb
MgKyaw <mgkyaw20112@gmail.com>
2025-06-22 21:17:50 +0700

    feat: Generate About component

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
   
-    HomeComponent
+    HomeComponent,
+       AboutComponent
   ],
   imports: [
     BrowserModule,
