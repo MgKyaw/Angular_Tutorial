c454cecb3d8e4397e0badf18599ff6c661128bd2
MgKyaw <mgkyaw20112@gmail.com>
2025-06-22 21:13:25 +0700

    feat: Render SharedModule

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule],
+  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
