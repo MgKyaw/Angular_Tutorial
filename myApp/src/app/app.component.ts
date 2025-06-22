c29ac62cb14aeb45fb64da72a426675ca138310b
MgKyaw <mgkyaw20112@gmail.com>
2025-06-22 21:36:31 +0700

    fix: Remove importing HomeComponent and AboutComponent at app.component.ts

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
 import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule, SharedModule, HomeComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
