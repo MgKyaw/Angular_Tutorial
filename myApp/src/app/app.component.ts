012da34775f7dc72b1c22843f93f3dcc9b2d979d
MgKyaw <mgkyaw20112@gmail.com>
2025-06-22 21:27:16 +0700

    feat: Import HomeComponent and AboutComponent

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterOutlet, AdminModule, RouterModule, SharedModule, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
