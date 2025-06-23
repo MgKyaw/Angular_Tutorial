92f6871b1bb83325f5554bf5f73ba38ae8e655cd
MgKyaw <mgkyaw20112@gmail.com>
2025-06-23 21:15:10 +0700

    feat: Define animations in app.component.ts

--- animation-app/src/app/app.component.ts
+++ animation-app/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('backgroundColorChange', [
+      state(
+        'default',
+        style({
+          backgroundColor: 'rgb(174, 5, 174)',
+        })
+      ),
+      state(
+        'hovered',
+        style({
+          backgroundColor: 'rgb(248, 174, 225)',
+        })
+      ),
+      transition('default <=> hovered', [animate('0.5s ease-in-out')]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'animation-app';
+  title = 'Animation Example';
+  isHovered = false;
 }
