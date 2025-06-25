b9ec24b6a5888cb1e5c9a765c1145c3e7e1c0615
MgKyaw <mgkyaw20112@gmail.com>
2025-06-25 17:17:46 +0700

    feat: Update myCalc.service.ts

--- singletonApp/src/app/my-calc.service.ts
+++ singletonApp/src/app/my-calc.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class MyCalcService {
 
   constructor() { }
+
+  add(n1: number, n2: number){
+    return n1 + n2;
+  }
+
+  subtract(n1: number, n2: number){
+    return n1 - n2;
+  }
+
+  multiply(n1: number, n2: number){
+    return n1 * n2;
+  }
+
+  divide(n1: number, n2: number){
+    return n1 / n2;
+  }
+
 }
