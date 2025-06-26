067c666206fc2973e5c092c79f14380b5454affc
MgKyaw <mgkyaw20112@gmail.com>
2025-06-26 22:09:06 +0700

    feat: Implement math methods by using DI

--- singletonApp/src/app/calculator/calculator.component.ts
+++ singletonApp/src/app/calculator/calculator.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MyCalcService } from '../my-calc.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
-export class CalculatorComponent {
+export class CalculatorComponent{
+  //injecting service
+  constructor(private myservice: MyCalcService){}
+  n1: number = 0;
+  n2: number = 0;
+  result : any = "";
+
+   add(){
+    this.result = "Sum is (n1+n2): " + this.myservice.add(this.n1, this.n2);
+   }
+   subtract(){
+    this.result = "Subtraction is(n1-n2): " + this.myservice.subtract(this.n1, this.n2);
+   }
+   multiply(){
+    this.result = "Multiplication is(n1xn2): " + this.myservice.multiply(this.n1, this.n2);
+   }
+   divide(){
+    this.result = "Division is(n1/n2): " + this.myservice.divide(this.n1, this.n2);
+   }
 
 }
