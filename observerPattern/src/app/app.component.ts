84182592eefca66d7cdbfa89f2346ee96339b829
MgKyaw <mgkyaw20112@gmail.com>
2025-06-27 18:22:25 +0700

    feat: Inject observer service into app component

--- observerPattern/src/app/app.component.ts
+++ observerPattern/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ObserverService } from './observer.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'observerPattern';
+export class AppComponent{
+  constructor(private myService: ObserverService) {}
 }
