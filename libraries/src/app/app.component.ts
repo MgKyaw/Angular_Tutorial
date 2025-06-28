233fc0ee920722769dc89f3db178835d923acf26
MgKyaw <mgkyaw20112@gmail.com>
2025-06-28 21:59:47 +0700

    feat: Import primeng

--- libraries/src/app/app.component.ts
+++ libraries/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [MatInputModule],
+  imports: [MatInputModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
