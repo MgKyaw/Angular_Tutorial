12e0bd0a8269664ee3c59fda4f8d594846e8f65c
MgKyaw <mgkyaw20112@gmail.com>
2025-06-28 22:03:53 +0700

    feat: Add event listener for click

--- libraries/src/app/app.component.ts
+++ libraries/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { InputTextModule } from 'primeng/inputtext';
+import { fromEvent } from 'rxjs';
+fromEvent(document, 'click').subscribe(() => console.log('You Clicked!'));
 
 @Component({
   selector: 'app-root',
