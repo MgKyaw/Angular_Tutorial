0eece9d809fbf2e24f03deb0015cb6f480e7e0ff
MgKyaw <mgkyaw20112@gmail.com>
2025-06-29 13:08:48 +0700

    feat: Use pipe for event listeners

--- RxJS-app/src/app/app.component.ts
+++ RxJS-app/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { fromEvent } from 'rxjs';
+import { fromEvent, scan } from 'rxjs';
 
-fromEvent(document, 'click').subscribe(() => console.log('You Clicked!'));
+fromEvent(document, 'click')
+  .pipe(scan((count) => count + 1, 0))
+  .subscribe((count) => console.log('Clicked ${count} times'));
 
 @Component({
   selector: 'app-root',
