5801bb5cee274373cb9d6245bcec49c3e8f49fc5
MgKyaw <mgkyaw20112@gmail.com>
2025-07-01 22:22:03 +0700

    feat: Create an obserable

--- RxJS-app/src/app/app.component.ts
+++ RxJS-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { fromEvent, scan } from 'rxjs';
 import { from, Observable } from 'rxjs';
+import { interval } from 'rxjs';
 
 fromEvent(document, 'click')
   .pipe(scan((count) => count + 1, 0))
@@ -14,6 +15,12 @@ data.subscribe({
   error(err) { console.error('Error: ' + err); },
   complete() { console.log('Completed'); }
 })
+
+const secondsCounter = interval(1000);
+
+const subscription = secondsCounter.subscribe(n => 
+  console.log(`It's been ${n + 1} seconds since the subscription started!`)
+);
 
 @Component({
   selector: 'app-root',
