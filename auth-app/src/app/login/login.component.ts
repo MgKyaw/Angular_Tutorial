2e1b2b7f9c7d17af02ab7d3529f782f352e43fd7
MgKyaw <mgkyaw20112@gmail.com>
2025-07-02 20:24:20 +0700

    feat: Update login component

--- auth-app/src/app/login/login.component.ts
+++ auth-app/src/app/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../Services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+   selector: 'app-login',
+   standalone: true,
+   imports: [ReactiveFormsModule],
+   templateUrl: './login.component.html',
+   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+    userName: string = "";
+    password: string = "";
+    formData!: FormGroup;
 
+    constructor(private authService : AuthService, private router : Router) { }
+
+    ngOnInit() {
+      this.formData = new FormGroup({
+         userName: new FormControl("admin"),
+         password: new FormControl("admin"),
+      });
+    }
+
+    onClickSubmit(data: any) {
+      this.userName = data.userName;
+      this.password = data.password;
+
+      console.log("Login page: " + this.userName);
+      console.log("Login page: " + this.password);
+
+      this.authService.login(this.userName, this.password)
+         .subscribe( data => { 
+            console.log("Is Login Success: " + data); 
+            if(data) this.router.navigate(['/expenses']); 
+      });
+    }
 }
