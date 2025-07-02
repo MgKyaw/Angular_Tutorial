1f876b7bcb5145d793f6c7a3cd4328a4d439117a
MgKyaw <mgkyaw20112@gmail.com>
2025-07-02 20:40:55 +0700

    feat: Update AppComponent

--- auth-app/src/app/app.component.ts
+++ auth-app/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AuthService } from './Services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ExpenseEntryListComponent, LoginComponent, LogoutComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'auth-app';
+    title = 'Angular Authentication';
+    isUserLoggedIn: any = false;
+    router: any;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+      // Subscribe to the isUserLoggedIn observable from AuthService
+      this.authService.isUserLoggedIn$.subscribe(status => {
+        this.isUserLoggedIn = status;
+        console.log("Is User Logged In: ", this.isUserLoggedIn);
+      });
+    }
+    logout(): void {
+	   // Trigger logout in AuthService
+       this.authService.logout();
+	   // Redirect to the homepage after logout
+       this.router.navigate(['/']); 
+    }
 }
