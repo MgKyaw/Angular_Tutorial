00fc514d74880787e42268e9dbaff5caeb812867
MgKyaw <mgkyaw20112@gmail.com>
2025-07-02 20:34:36 +0700

    feat: Generate new component ExpenseEntryList

---
+++ auth-app/src/app/expense-entry-list/expense-entry-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-entry-list',
+  imports: [],
+  templateUrl: './expense-entry-list.component.html',
+  styleUrl: './expense-entry-list.component.css'
+})
+export class ExpenseEntryListComponent {
+
+}
