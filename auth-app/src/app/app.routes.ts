c8f540beb3cc3e1344f4feebc24a458033c67ad7
MgKyaw <mgkyaw20112@gmail.com>
2025-07-02 20:38:15 +0700

    feat: Update routes

--- auth-app/src/app/app.routes.ts
+++ auth-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { authenticateGuard } from './authenticate.guard';
+import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'expenses', component: ExpenseEntryListComponent, canActivate: [authenticateGuard]},
+    {path: ' ', redirectTo: '/login', pathMatch: 'full'}
+];
