22e0737dd8c3f3a76aa6461c0f609af54e97fa87
MgKyaw <mgkyaw20112@gmail.com>
2025-07-03 20:37:58 +0700

    feat: Import and use datePipe and localeHi module

--- i18n-sample/src/app/app.component.ts
+++ i18n-sample/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LOCALE_ID } from '@angular/core';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localeHi from '@angular/common/locales/hi';
+
+registerLocaleData(localeHi);
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, DatePipe],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'hi' }  
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'i18n-sample';
+  title = 'Internationalization Sample';
+  currentDate: number = Date.now();
 }
