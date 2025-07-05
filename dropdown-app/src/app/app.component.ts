69440a1d8e4bf7ca5773b4012f8327e4b6fea353
MgKyaw <mgkyaw20112@gmail.com>
2025-07-05 11:22:35 +0700

    feat: Import modules and implement dropdown material

--- dropdown-app/src/app/app.component.ts
+++ dropdown-app/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+   selector: 'app-root',
+   standalone: true,
+   imports: [RouterOutlet, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, CommonModule],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dropdown-app';
+   title = 'Dropdown Example';
+   selectedValue: string = "";
+   
+   options = [
+      { value: 'option1', viewValue: 'Red' },
+      { value: 'option2', viewValue: 'Saffron' },
+      { value: 'option3', viewValue: 'Violet' },
+      { value: 'option4', viewValue: 'Gray' }
+   ];
 }
