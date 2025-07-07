b0a3ef2c5db3be52ebaca12f057fed7b9b6ef354
MgKyaw <mgkyaw20112@gmail.com>
2025-07-07 21:21:58 +0700

    feat: Add myData array

--- displaying-data/src/app/app.ts
+++ displaying-data/src/app/app.ts
@@ -13,4 +13,21 @@ export class App {
   num: number = 10;
   isTrue: boolean = true;
   username: string = "mgkyaw.dev@gmail.com";
+  myData = [
+    {
+      "id": 1,
+      "name": 'abc',
+      "age": 24
+    },
+    {
+      "id": 2,
+      "name": "xyz",
+      "age": 20,
+    },
+    {
+      "id": 3,
+      "name": "red",
+      "age": 30
+    }
+  ];
 }
