c21c7df26148d1d386f7e8cc603d9f1faa1c6b38
MgKyaw <mgkyaw20112@gmail.com>
2025-07-08 20:38:11 +0700

    feat: Update expense-entry component

--- expense-manager-basic/src/app/expense-entry/expense-entry.ts
+++ expense-manager-basic/src/app/expense-entry/expense-entry.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-expense-entry',
-  imports: [],
-  templateUrl: './expense-entry.html',
-  styleUrl: './expense-entry.css'
+   selector: 'app-expense-entry',
+   standalone: true,
+   imports: [],
+   templateUrl: './expense-entry.component.html',
+   styleUrl: './expense-entry.component.css'
 })
-export class ExpenseEntry {
-
+export class ExpenseEntryComponent implements OnInit {
+   title: string = "";
+   constructor() { }
+   
+   ngOnInit() {
+      this.title = "Expense Entry"
+   }
 }
