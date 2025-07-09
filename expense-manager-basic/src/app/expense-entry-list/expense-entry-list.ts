a11d5d5875fa6cae96323d8af62f21b390481919
MgKyaw <mgkyaw20112@gmail.com>
2025-07-09 21:11:42 +0700

    feat: Implement logic on ExpenseEntryList

--- expense-manager-basic/src/app/expense-entry-list/expense-entry-list.ts
+++ expense-manager-basic/src/app/expense-entry-list/expense-entry-list.ts
@@ -1,11 +1,70 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-expense-entry-list',
-  imports: [],
-  templateUrl: './expense-entry-list.html',
-  styleUrl: './expense-entry-list.css'
-})
-export class ExpenseEntryList {
+export interface ExpenseEntry {
+   id: number;
+   item: string;
+   amount: number;
+   category: string;
+   location: string;
+   spendOn: Date;
+   createdOn: Date;
+}
 
+@Component({
+   selector: 'app-expense-entry-list',
+   standalone: true,
+   imports: [CommonModule],
+   templateUrl: './expense-entry-list.html',
+   styleUrl: './expense-entry-list.css'
+})
+export class ExpenseEntryListComponent {
+   getExpenseEntries() : ExpenseEntry[] { 
+     let mockExpenseEntries : ExpenseEntry[] = [ 
+        { id: 1, 
+           item: "Pizza", 
+           amount: Math.floor((Math.random() * 10) + 1), 
+           category: "Food", 
+           location: "Mcdonald", 
+           spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+           createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+        { id: 1, 
+           item: "Pizza", 
+           amount: Math.floor((Math.random() * 10) + 1), 
+           category: "Food", 
+           location: "KFC", 
+           spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+           createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+        { id: 1,
+           item: "Pizza",
+           amount: Math.floor((Math.random() * 10) + 1), 
+           category: "Food", 
+           location: "Mcdonald", 
+           spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+           createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+        { id: 1, 
+           item: "Pizza", 
+           amount: Math.floor((Math.random() * 10) + 1), 
+           category: "Food", 
+           location: "KFC", 
+           spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+           createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+        { id: 1, 
+           item: "Pizza", 
+           amount: Math.floor((Math.random() * 10) + 1), 
+           category: "Food", 
+           location: "KFC", 
+           spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+           createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) 
+        }, 
+     ]; 
+     return mockExpenseEntries; 
+   }
+   title: string =""; 
+   expenseEntries!: ExpenseEntry[]; 
+   constructor() { } 
+   ngOnInit() { 
+      this.title = "Expense Entry List"; 
+      this.expenseEntries = this.getExpenseEntries(); 
+   }
 }
