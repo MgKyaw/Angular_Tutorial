6309f8499fa6f5f97dd5b062d264921ce553fe1e
MgKyaw <mgkyaw20112@gmail.com>
2025-07-09 21:16:17 +0700

    feat: Import ExpenseEntryList

--- expense-manager-basic/src/app/app.ts
+++ expense-manager-basic/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExpenseEntry } from './expense-entry/expense-entry';
+import { ExpenseEntryList } from './expense-entry-list/expense-entry-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExpenseEntry],
+  imports: [RouterOutlet, ExpenseEntry, ExpenseEntryList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
