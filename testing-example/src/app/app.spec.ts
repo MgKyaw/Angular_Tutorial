2bd20a6c72d466fce6656acdc7dc48b52b91cb78
MgKyaw <mgkyaw20112@gmail.com>
2025-07-10 21:01:56 +0700

    feat: Update app.spec.ts

--- testing-example/src/app/app.spec.ts
+++ testing-example/src/app/app.spec.ts
@@ -3,23 +3,31 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()]
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing-example');
-  });
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+         imports: [
+            provideZonelessChangeDetection
+         ],
+         declarations: [
+            App
+         ],
+      }).compileComponents();
+   }));
+   it('should create the app', () => {
+      const fixture = TestBed.createComponent(App);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+   });
+   it(`should have as title 'angular7-app'`, () => {
+      const fixture = TestBed.createComponent(App);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('angular7-app');
+   });
+   it('should render title in a h1 tag', () => {
+      const fixture = TestBed.createComponent(App);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain(
+         'Welcome to angular7-app!');
+   })
 });
