ded352896ff9027837dd74a6cc7266eef0a246ac
MgKyaw <mgkyaw20112@gmail.com>
2025-07-10 20:50:54 +0700

    feat: Update RegistrationComponent

--- error-handling/src/app/registration-component/registration-component.ts
+++ error-handling/src/app/registration-component/registration-component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-registration-component',
-  imports: [],
-  templateUrl: './registration-component.html',
-  styleUrl: './registration-component.css'
+   selector: 'app-registration',
+   standalone: true,
+   imports: [CommonModule],
+   templateUrl: './registration.component.html',
+   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-
+   registrationForm: FormGroup;
+   
+   constructor(private fb: FormBuilder) {
+      this.registrationForm = this.fb.group({
+         username: ['', [Validators.required, Validators.minLength(4)]],
+         email: ['', [Validators.required, Validators.email]],
+      });
+   }
+   
+   onSubmit() {
+      if (this.registrationForm.invalid) {
+         this.showValidationErrors();
+         return;
+      }
+   }
+   
+   private showValidationErrors() {
+      Object.keys(this.registrationForm.controls).forEach(control => {
+         const formControl = this.registrationForm.get(control);
+         if (formControl?.invalid) {
+            console.error(`${control} is invalid`);
+         }
+      });
+   }
 }
