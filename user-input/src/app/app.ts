89021317fec0d2b2f610646e117ab2149fd4f7f2
MgKyaw <mgkyaw20112@gmail.com>
2025-07-11 22:39:59 +0700

    feat: Add submitForm method

--- user-input/src/app/app.ts
+++ user-input/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'user-input';
+
+  user = { name: '', email: ''};
+
+  submitForm() {
+    console.log(this.user);
+  }
 }
