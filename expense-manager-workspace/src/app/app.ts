bc6b8909804930568364ecae73772564633d8a39
MgKyaw <mgkyaw20112@gmail.com>
2025-07-12 20:36:48 +0700

    feat: Update title to "Expense Manager"

--- expense-manager-workspace/src/app/app.ts
+++ expense-manager-workspace/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'expense-manager-workspace';
+  protected title = 'Expense Manager';
 }
