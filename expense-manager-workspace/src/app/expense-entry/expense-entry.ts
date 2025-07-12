cf2cafa81549560d37d1ca5c18a082c9424a2369
MgKyaw <mgkyaw20112@gmail.com>
2025-07-12 20:38:19 +0700

    feat: Generate new component expense-entry

---
+++ expense-manager-workspace/src/app/expense-entry/expense-entry.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-entry',
+  imports: [],
+  templateUrl: './expense-entry.html',
+  styleUrl: './expense-entry.css'
+})
+export class ExpenseEntry {
+
+}
